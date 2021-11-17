988eb3a2572f528034a281a32833b6a7af231349
Victor <ua6em@yandex.ru>
2021-11-17 22:48:07 +0300

    Update iBUS_Demo.ino

--- examples/iBUS_Demo.ino
+++ examples/iBUS_Demo.ino
@@ -2,18 +2,32 @@
  * Test FlySky IBus interface on an Arduino Mega.
  *  Connect FS-iA6B receiver to Serial1.
  */
- 
+
 #include <SomeSerial.h>
 #include "iBUSSome.h"
+
+#define SOFTPORT  // если определено используем SofwareSerial
+#define SERIAL1   //328PB ONLY
+
+#ifdef  SOFTPORT
+SomeSerial IBusPort(2, 3); // RX, TX
+iBUSSome IBusSerial(IBusPort);
+#else
+  #ifdef SERIAL1
+  SomeSerial IBusSerial(&Serial1);
+  #else
+  SomeSerial IBusSerial(&Serial);
+  #endif
+#endif 
 
 void setup() 
 {
   Serial.begin(115200);
-  IBus.begin(Serial1);
+  IBusSerial.begin(Serial1);
 }
 
 void loop() 
 {
-  IBus.loop();
-  Serial.println(IBus.readChannel(0), HEX);
+ IBusSerial.loop();
+  Serial.println(IBusSerial.readChannel(0), HEX);
 }
